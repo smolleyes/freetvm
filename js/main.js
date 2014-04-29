5bbf0205fe2041e7b3c1df9a760ffc3e4bfe322f
Smolleyes <s.lagui@free.fr>
2014-04-29 23:56:09 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,474 @@
+//node modules
+var fs = require('fs'),
+    path = require('path'),
+    http = require('http'),
+    cpu = require('os-utils'),
+    nodeip = require('node-ip'),
+    deviceType = require('ua-device-type'),
+    url = require('url'),
+    spawn = require('child_process').spawn;
+
+// node-webkit window
+var gui = require('nw.gui');
+var win = gui.Window.get();
+win.setResizable(false);
+win.on('loaded', function() {
+    this.show();
+    this.setPosition("center");
+});
+
+//globals
+VERSION="0.1";
+var exec_path=path.dirname(process.execPath);
+var winIshidden = true;
+var megaServer;
+var storage = localStorage;
+var settings = {};
+var ffmpeg;
+var ffar = [];
+
+// Create a tray icon
+var tray = new gui.Tray({ title: 'FreeTvM-server', icon: 'img/icon.png' });
+
+// Give it a menu
+var menu = new gui.Menu();
+menu.append(new gui.MenuItem({ label: 'Quitter' }));
+tray.menu = menu;
+menu.items[0].click = function() { 
+    win.hide();
+    win.close(true);
+};
+
+// on tray click
+tray.on('click',function(e){
+    if(winIshidden){
+        win.show();
+        winIshidden = false;
+    } else {
+        win.hide();
+        winIshidden = true;
+    }
+});
+
+// start app
+$(document).on('ready',function(e){
+    //load stored infos
+    if (storage.ftvSettings === undefined) {
+        settings.ip = nodeip.address();
+        settings.version = VERSION;
+        settings.sharedFolders = [];
+        storage.ftvSettings = JSON.stringify(settings);
+    } else {
+        settings = JSON.parse(storage.ftvSettings);
+    }
+    // start keyevent listener
+    fn = function(e){ onKeyPress(e); };
+    document.addEventListener("keydown", fn, false );
+    //start server
+    startMegaServer();
+    // show window when ready
+    winIshidden = false;
+
+    // collapsible div
+    $(document).on('click', '.panel-heading span.clickable', function(e){
+        var $this = $(this);
+        if(!$this.hasClass('panel-collapsed')) {
+            $this.parents('.panel').find('.panel-body').slideUp();
+            $this.parents('.panel').find('.panel-footer').slideUp();
+            $this.addClass('panel-collapsed');
+            $this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+
+        } else {
+            $this.parents('.panel').find('.panel-body').slideDown();
+            $this.parents('.panel').find('.panel-footer').slideDown();
+            $this.removeClass('panel-collapsed');
+            $this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+        }
+    }); 
+    // add stored folders to the list
+    var list = settings.sharedFolders;
+    var length = list.length;
+    if (length !== 0) {
+        $('#foldersList').empty();
+        $.each(list,function(index,folder){
+            addFolder(folder);
+        });
+    }
+    // add folder
+    $("#addFolderBtn").click(function(e){
+        e.preventDefault();
+        chooseFile("#fileDialog");
+    });
+    // remove folder
+    $(document).on("click",".removeFolderBtn",function(e){
+        e.preventDefault();
+        var folder = $(this).attr('data').trim();
+        var index = $.inArray(folder, settings.sharedFolders);
+        if (index>=0) settings.sharedFolders.splice(index, 1);
+        $(this).parent().remove();
+        storage.ftvSettings = JSON.stringify(settings);
+        if($(".removeFolderBtn").length === 0) {
+            $('#foldersList').empty().append('<li class="list-group-item">Aucun dossier partagé...</li>');
+        }
+    });
+});
+
+// open fileDialog and save result in localStorage
+function chooseFile(name) {
+    var chooser = $(name);
+    chooser.on("change",function(evt) {
+        var folder = $(this).val();
+        if($.inArray(folder,settings.sharedFolders) == -1) {
+            settings.sharedFolders.push(folder);
+            storage.ftvSettings = JSON.stringify(settings);
+            addFolder(folder);
+        }
+    });
+    chooser.click();
+}
+
+// add folder function
+function addFolder(folder){
+    var list = $(".removeFolderBtn");
+    if(list.length === 0) {
+        $('#foldersList').empty();
+    }
+    var html ='<li class="list-group-item">'+path.basename(folder)+' \
+                    <a href="#" data='+folder+' class="btn btn-sm btn-danger pull-right glyphicon glyphicon-minus removeFolderBtn"></a> \
+                </li>';
+    $('#foldersList').append(html);
+}
+
+// Start server
+function startMegaServer() {
+    try {
+        megaServer.close();
+    } catch(err) {
+    megaServer = http.createServer(function (req, res) {
+        console.log("[DEBUG] request received : "+decodeURIComponent(req.url)+"\n");
+        if((req.url !== "/favicon.ico") && (req.url !== "/")) {
+            if (req.url.indexOf("/getPlaylist") !== -1) {
+                var html="";
+                var json = {};
+                json.channels = [];
+                $.get('http://mafreebox.freebox.fr/freeboxtv/playlist.m3u',function(resp){
+                    var list = resp.split('#EXTINF');
+                    $.each(list,function(index,c){
+                        var chaine = c.trim().replace(/(\r\n|\n|\r)/gm,"");
+                        var infos = {};
+                        try {
+                            infos.canal = chaine.split(" ")[0].split(",")[1];
+                            infos.link = 'rtsp://'+chaine.match(/rtsp:\/\/(.*)/)[1];
+                            var n = chaine.match(/(.*?)-(.*?)\)/)[2]+(')');
+                            infos.name = n.trim();
+                            infos.thumb = 'img/fbxLogos/'+infos.canal+'.png';
+                            json.channels.push(infos);
+                            var link =  'http://'+req.headers["host"]+'/?file='+infos.link+'&tv';
+                            html+='<a class="tvLink" href="#" src="'+link+'" style="decoration:none;">'+infos.name+'</a><br>';
+                            if (index+1 === list.length) {
+                                if (req.url.indexOf("json") !== -1){
+                                    var body = JSON.stringify(json);
+                                    res.writeHead(200, {"Content-Type": "application/json;charset=utf-8"});
+                                    res.end(body);
+                                } else {
+                                    res.writeHead(200,{'Content-type': 'text/html'});
+                                    res.end(html, 'utf-8');
+                                }
+                            }
+                        } catch(err){
+                            console.log("n'est pas une chaine", err);
+                        }
+                    });
+                });
+            } else if (req.url.indexOf("/getLocalDbJson") !== -1){
+                getLocalDb(res);
+            } else if (req.url.indexOf("/test") !== -1){
+                res.writeHead(200, {"Content-Type": 'text/html'});
+                res.write('ok!');
+                res.end();
+            } else {
+                getMetadata(req,res);
+                //startStreaming(req,res);
+            }
+        }
+    }).listen(8888);
+    $("#serverStateImg").attr('src','img/online.png');
+    $("#serverState").empty().append('Serveur en écoute sur '+nodeip.address()+' port 8888');
+    }
+}
+
+
+////////////////////////////////////////////////
+// list local shared folders
+
+function getLocalDb(res) {
+    try {
+        var dirs = settings.sharedFolders;
+        if ((dirs === undefined) || (dirs.length === 0)) {
+            fileList.basedirs[dir] = {};
+            return;
+        }
+    } catch(err) {
+        console.log("shared dirs error : "+ err);
+        fileList.basedirs[dir] = {};
+        return;
+    }
+    var fileList = [];
+    var total = dirs.length;
+    $.each(dirs,function(index,dir){
+        if (dir === "") {
+            if (index+1 === dirs.length) {
+                var body = JSON.stringify(fileList);
+                res.writeHead(200, {"Content-Type": "application/json;charset=utf-8"});
+                res.end(body);
+            }
+            return true;
+        } else {
+            fileList.push(dirTree(dir));
+            if (index+1 === dirs.length) {
+                var body = JSON.stringify(fileList);
+                res.writeHead(200, {"Content-Type": "application/json;charset=utf-8"});
+                res.end(body);
+            }
+        }
+    }); 
+}
+
+function dirTree(filename) {
+    var stats = fs.lstatSync(filename),
+    info = {
+        path: filename,
+        name: path.basename(filename)
+    };
+    if (stats.isDirectory()) {
+        info.type = "folder";
+        info.children = fs.readdirSync(filename).map(function(child) {
+            return dirTree(filename + '/' + child);
+        });
+    } else {
+        // Assuming it's a file. In real life it could be a symlink or
+        // something else!
+        info.type = "file";
+    }
+    return info;
+}
+
+//////////////////////////////////////////////
+// get ffprobe metadata
+
+function getMetadata(req,res){
+    var ffprobe;
+    var link;
+    var bitrate = '';
+    var resolution = '';
+    var parsedLink = decodeURIComponent(url.parse(req.url).href);
+    try {
+        link = parsedLink.match(/\?file=(.*?)&tv/)[1].replace(/\+/g,' ');
+    } catch(err) {
+        try {
+            link = parsedLink.match(/\?file=(.*)/)[1].replace(/\+/g,' ');
+        } catch(err) {
+            res.end();
+            return;
+        }
+    }
+    var args = ['-show_streams',link];
+    if (process.platform === 'win32') {
+        ffprobe = spawn(exec_path+'/ffprobe.exe', args);
+    } else {
+        ffprobe = spawn(exec_path+'/ffprobe', args);
+    }
+    ffprobe.stderr.on('data', function(data) {
+        var infos = data.toString();
+        try{
+            if (resolution === '') {
+                var vinfos = infos.match(/Video:(.*)/)[1];
+                console.log(vinfos)
+                resolution = vinfos.toLowerCase().match(/\d{3}(?:\d*)?x\d{3}(?:\d*)/)[0];
+            }
+        }catch(err){
+
+        }
+    });
+    ffprobe.on('exit', function(data) {
+        console.log('[DEBUG] ffprobe exited...'+bitrate+' '+resolution);
+        var width = 640;
+        var height = 480;
+        width = parseInt(resolution.split("x")[0]);
+        height = parseInt(resolution.split("x")[1]);
+        startStreaming(req,res,width,height)
+    });
+}
+
+///////////////////////////////////////////////
+// streaming function
+function startStreaming(req,res,inwidth,inheight) {
+    try {
+        cleanffar();
+    } catch(err) {
+        console.log(err);
+    }
+    try {
+        var link;
+        var baseLink = url.parse(req.url).href;
+        var parsedLink = decodeURIComponent(url.parse(req.url).href);
+        var device = deviceType(req.headers['user-agent']);
+        var linkParams = parsedLink.split('&');
+        var bitrate = 300;
+        var swidth=inwidth;
+        var sheight=inheight;
+        var host = req.headers['host'];
+        var args;
+        //get link
+        try {
+            link = parsedLink.match(/\?file=(.*?)&tv/)[1].replace(/\+/g,' ');
+        } catch(err) {
+            link = parsedLink.match(/\?file=(.*)/)[1].replace(/\+/g,' ');
+        }
+        console.log("[DEBUG] Opening link: " + link)
+        //get screen dimensions from request
+        try {
+            swidth = parseInt(linkParams.slice(-1)[0].replace('screen=',"").split('x')[0]);
+            sheight = parseInt(linkParams.slice(-1)[0].replace('screen=',"").split('x')[1]);
+            if (swidth > inwidth) {
+                swidth = inwidth;
+                sheight = inheight;
+            }
+            console.log("[DEBUG] Video size: " + swidth + "x" + sheight);
+        } catch(err) {
+            console.log("[DEBUG] no width/height specified...using "+inwidth+'x'+inheight);
+        }
+        // get bitrate
+        if (parsedLink.indexOf('&bitrate') !== -1){
+            try {
+              bitrate = parsedLink.match(/&bitrate=(.*?)&/)[1];
+            } catch(err) {
+              bitrate = parsedLink.match(/&bitrate=(.*)/)[1];
+            }
+            console.log("[DEBUG] Bitrate:" + bitrate + "k");
+        }
+        // set response headers
+        res.writeHead(200, {
+            'Connection':'keep-alive',
+            'Content-Type': 'video/mp4',
+            'Server':'Ht5treamer/0.0.1'
+        });
+        // if local file
+        var ffmpeg;
+        if (link.indexOf('file:') !== -1) {
+            res.writeHead(200, {
+                'Content-Type': 'video/mp4'
+            });
+            var link = link.replace('file://','');
+            ffmpeg = spawnFfmpeg(link,device,host,bitrate,swidth,sheight,function (code) { // exit
+              $("#serverStats").empty().append("<span>ERREUR FFMPEG:<br>"+e+"</span>");
+              res.end();
+            });
+            var x = fs.createReadStream(link).pipe(ffmpeg.stdin);
+            x.on('error',function(err) {
+                  console.log('ffmpeg stdin error...' + err);
+                  if (err.stack.indexOf('codec') === -1) {
+                    console.log("Arret demandé !!!");
+                    res.end();
+                  }
+            });
+            ffmpeg.stdout.pipe(res);
+        } else {
+            // start ffmpeg
+            ffmpeg = spawnFfmpeg(link,device,host,bitrate,swidth,sheight,function (code) { // exit
+                res.end();
+            });
+            ffmpeg.stdout.pipe(res);
+        }
+
+        ffmpeg.on('exit', function() {
+                console.log('[DEBUG] FFmpeg stopped...');
+                ffmpeg.kill('SIGKILL');
+                res.end();
+                setTimeout(function(){
+                    $("#serverStats").empty().append("<span>En attente...</span>");
+                },2000);
+        });
+              
+        ffmpeg.stderr.on('data', function (data) {
+            updateStats(data.toString(),swidth,sheight,bitrate);
+            console.log(data.toString());
+        });
+
+        ffmpeg.on('error',function(e) {
+            console.log(e);
+            res.end();
+            ffmpeg.kill('SIGKILL');
+            $("#serverStats").empty().append("<span>ERREUR FFMPEG:<br>"+e+"</span>");
+        });
+
+    } catch(err) {
+        console.log("[DEBUG] ERROR in startStreaming function: " + err);
+        res.end();
+    }
+    res.on("close",function(){
+        ffmpeg.kill('SIGKILL');
+        setTimeout(function(){
+            $("#serverStats").empty().append("<span>En attente...</span>");
+        },2000);
+    });
+}
+
+function spawnFfmpeg(link,device,host,bitrate,swidth,sheight,exitCallback) {
+    if (host.match(/(^127\.)|(^192\.168\.)|(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^::1$)/) !== null) {
+        args = ['-i',link,'-f','matroska','-sn','-c:v', 'libx264','-preset', 'fast','-deinterlace',"-aspect", "16:9","-b:v",bitrate+"k",'-c:a', 'libopus','-b:a','256k','-threads', '0', '-'];
+    } else {
+        args = ['-i',link,'-f','matroska','-sn','-c:v', 'libx264','-preset', 'fast','-deinterlace',"-aspect", "16:9","-b:v",bitrate+"k",'-c:a', 'libopus','-b:a','64k','-threads', '0', '-'];
+    }
+    console.log('[DEBUG] Starting ffmpeg:\n' + args.join(' '));
+    if (process.platform === 'win32') {
+        ffmpeg = spawn(exec_path+'/ffmpeg.exe', args);
+    } else {
+        ffmpeg = spawn(exec_path+'/ffmpeg', args);
+    }
+    ffar.push(ffmpeg);
+    return ffmpeg;
+}
+
+function updateStats(infos,width,height) {
+    var data = infos;
+    cpu.cpuUsage(function(v){
+        var cpuStats = ( 'Utilisation cpu: ' + (v*100).toFixed(2) + "%" );
+        try {
+            var infos = '';
+            infos += "<b>infos vidéos:</b><br>";
+            infos += "Résolution: "+width+'x'+height+'<br><br>';
+            infos += "<b>statistiques d'encodage:</b><br>";
+            infos += "Durée encodage: "+ data.match(/time=(.*?)bitrate/)[1].trim()+"<br>";
+            infos += "Qualité: "+ data.match(/bitrate=(.*?)kb/)[1].trim()+"kbits/s<br>";
+            infos += "Total encodé: "+ data.match(/size=(.*?)time/)[1].trim()+"<br>";
+            infos += "Images/secondes: "+ data.match(/fps=(.*?)q/)[1].trim()+"<br>";
+            $("#serverStats").empty().append("<span>"+infos+"</span><span>"+cpuStats+"</span>");
+        } catch(err) {
+            $("#serverStats").empty().append("<span>Streaming audio en cours...<span><br>"+cpuStats+"</span>");
+            return;
+        }
+    });
+}
+
+
+function cleanffar() {
+    $.each(ffar,function(index,ff){
+      try{
+        ff.kill("SIGKILL");
+      } catch(err){}
+      if (index+1 === ffar.length){
+        ffar = [];
+      }
+    });
+}
+
+////////////////////////////////////////////////
+// onKeypress EventListener
+
+function onKeyPress(key) {
+    if (key.key === 'd') {
+        key.preventDefault();
+        win.showDevTools();
+    }
+}
---
+++ js/notify.js
@@ -0,0 +1,155 @@
+var http=require('http');
+var path = require('path');
+var fs = require('fs');
+var temp = require('temp');
+var execFile = require('child_process').exec;
+var execDir;
+var online_version;
+
+$(document).ready(function(){
+    try {
+        http.get('http://sd-20470.dedibox.fr/freetvm/update.html',function(res,err){
+            var datas = [];
+            res.on('data',function(chunk){
+                datas.push(chunk);
+            });
+            res.on('end',function(){
+                console.log("Checking for updates....");
+                var data = datas.join('');
+                var txt = $('p',data).prevObject[1].innerHTML;
+                online_version = txt;
+                try {
+					console.log("online version : "+online_version+', current version : '+ settings.version);
+				} catch(err){
+					console.log(err);
+					return;
+				}
+                if (online_version === settings.version) {
+                    $.notif({title: 'FreeTv-M:',cls:'success',icon: '&#10003;',content:"FreeTv-M est à jour !",btnId:'',btnTitle:'',btnColor:'',btnDisplay: 'none',updateDisplay:'none'});
+                    
+                } else {
+                    $.notif({title: 'FreeTv-M:',cls:'error',icon: '&#59256;',timeout:0,content:"Une nouvelle version de freeTv-M est disponible !",btnId:'updateBtn',btnTitle:'Mise à jour',btnColor:'black',btnDisplay: 'block',updateDisplay:'none'})
+                }
+            });
+        });
+    } catch (err) {
+        console.log("offline mode or update server down....");
+    }
+    
+    // udpates
+    $(document).on('click','#updateBtn',function(e) {
+        e.preventDefault();
+        var arch = process.arch;
+        var file = '';
+        var link = '';
+        if (process.platform === 'win32') {
+            file = 'freetvm-setup.exe';
+            link = 'http://sd-20470.dedibox.fr/freetv/windows/'+file;
+		} else if (process.platform === 'darwin') {
+			 file = 'freetvm-osx.zip';
+			 link = 'http://www.ubukey.fr/freetvm/osx/'+file;
+        } else {
+            if (arch === 'ia32') {
+                console.log('linux 32 bits detected...');
+                file = 'freetvm-32.zip';
+            } else if (arch === 'x64') {
+                console.log('linux 64 bits detected...');
+                file = 'freetvm-64.zip';
+            }
+            link = 'http://sd-20470.dedibox.fr/ht5streamer/'+file;
+        }
+        downloadUpdate(link,file);
+    });
+});
+
+function downloadUpdate(link,filename) {
+    $.notif({title: 'Mise à jour de FreeTv-M:',icon: '&#128229;',timeout:0,content:'',btnId:'',btnTitle:'',btnColor:'',btnDisplay: 'none',updateDisplay:'block'});
+    // remove file if already exist
+    var pbar = $('#updateProgress');
+    execDir = path.dirname(process.execPath);
+    // start download
+    $('#updateProgress strong').html("En attente de connexion...");
+    var val = $('#updateProgress progress').attr('value');
+    var currentTime;
+    var startTime = (new Date()).getTime();
+    current_download = http.request(link,
+    function (response) {
+	var contentLength = response.headers["content-length"];
+    if (parseInt(contentLength) === 0) {
+		$('#updateProgress strong').html("Impossible de télécharger la mise à jour...");
+		setTimeout(function(){pbar.hide()},5000);
+    }
+    temp.mkdir('ht5streamer', function(err, dirPath) {
+	tmpPath = dirPath;
+	var target;
+	if (process.platform === 'win32') {
+		target = dirPath+'\\'+filename;
+	} else {
+		target = dirPath+'/'+filename;
+	}
+	var file = fs.createWriteStream(target);
+	response.on('data',function (chunk) {
+		file.write(chunk);
+		var bytesDone = file.bytesWritten;
+		currentTime = (new Date()).getTime();
+		var transfer_speed = (bytesDone / ( currentTime - startTime)).toFixed(2);
+		var newVal= bytesDone*100/contentLength;
+		var txt = Math.floor(newVal)+'% '+'effectué à'+' '+transfer_speed+' kb/s';
+		$('#updateProgress progress').attr('value',newVal).text(txt);
+		$('#updateProgress strong').html(txt);
+	});
+	response.on('end', function() {
+	    file.end();
+	    $('#updateProgress b').empty();
+	    $('#updateProgress strong').html('Téléchargement terminé !');
+	    $('#updateProgress progress').hide();
+	    var execDir = path.dirname(process.execPath);
+	    var update;
+	    process.chdir(tmpPath);
+	    $('#updateProgress strong').html('Installation de la mise à jour...');
+	    
+	    if (process.platform === 'win32') {
+			var f = tmpPath.replace(/\\/g,"\\\\")+'\\\\freetvm-setup.exe';
+			var exe = exec(f); 
+			setTimeout(function(){win.emit('close')},2000);
+ 	    } else if (process.platform === 'darwin') {
+			var dest = path.dirname(execDir.match(/(.*)freetvm.app(.*?)/)[0]);
+		    var args = ['-o',filename,'-d',dest];
+		    var update = spawn('unzip', args);
+	    	update.on('exit', function(data){
+		    	pbar.click();
+		    	$('.notification').click();
+		    	if (parseInt(data) == 0) {
+			    	$.notif({title: 'Ht5streamer:',cls:'green',timeout:10000,icon: '&#10003;',content:"Mise à jour installée avec succès! merci de relancer freeTv-M",btnId:'',btnTitle:'',btnColor:'',btnDisplay: 'none',updateDisplay:'none'});
+		    	} else {
+			    	$.notif({title: 'Ht5streamer:',cls:'red',timeout:10000,icon: '&#10006;',content:"Erreur de mise à jour, merci de remonter le problème... essayez de reinstaller manuellement!",btnId:'',btnTitle:'',btnColor:'',btnDisplay: 'none',updateDisplay:'none'});
+		    	}
+	    	});
+	    	update.stderr.on('data', function(data) {
+		    	$('.notification').click();
+		    	$.notif({title: 'Ht5streamer:',cls:'red',timeout:10000,icon: '&#10006;',content:"Erreur de mise à jour, merci de remonter le problème... essayez de reinstaller manuellement!" + data,btnId:'',btnTitle:'',btnColor:'',btnDisplay: 'none',updateDisplay:'none'});
+		    	console.log('update stderr: ' + data);
+	    	});
+ 	    } else {
+		    var args = ['-o',filename,'-d',execDir];
+		    var update = spawn('unzip', args);
+	    	update.on('exit', function(data){
+		    	pbar.click();
+		    	$('.notification').click();
+		    	if (parseInt(data) == 0) {
+			    	$.notif({title: 'Ht5streamer:',cls:'green',timeout:10000,icon: '&#10003;',content:"Mise à jour installée avec succès! merci de relancer freeTv-M",btnId:'',btnTitle:'',btnColor:'',btnDisplay: 'none',updateDisplay:'none'});
+		    	} else {
+			    	$.notif({title: 'Ht5streamer:',cls:'red',timeout:10000,icon: '&#10006;',content:"Erreur de mise à jour, merci de remonter le problème... essayez de reinstaller manuellement!",btnId:'',btnTitle:'',btnColor:'',btnDisplay: 'none',updateDisplay:'none'});
+		    	}
+	    	});
+	    	update.stderr.on('data', function(data) {
+		    	$('.notification').click();
+		    	$.notif({title: 'Ht5streamer:',cls:'red',timeout:10000,icon: '&#10006;',content:"Erreur de mise à jour, merci de remonter le problème... essayez de reinstaller manuellement!",btnId:'',btnTitle:'',btnColor:'',btnDisplay: 'none',updateDisplay:'none'});
+		    	console.log('update stderr: ' + data);
+	    	});
+		}
+	});
+    });
+});
+current_download.end();
+}
